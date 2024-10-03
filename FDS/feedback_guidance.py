0c89993a9adf3431a73c8a0511c3a82deef819b6
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2024-10-03 10:48:03 +0100

    Update feedback_guidance.py

--- FDS/feedback_guidance.py
+++ FDS/feedback_guidance.py
@@ -1,7 +1,8 @@
 exercises = [
       {
         "id": "homework1_Q1",
-        "guidance": """Their work should be similar to this with thisL The most important thing is the value of characters_q1 must be set to the current answer option (3). Your response should not give away the actual answer. Only confirm Validity:
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of characters_q1 must be set to the correct answer option which is 3. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 3:
+
                     def hw1_exercise_1():
                       characters_q1 = 3
                       return characters_q1
