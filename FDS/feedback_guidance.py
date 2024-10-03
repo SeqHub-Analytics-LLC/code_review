f590fafcd2958ee60422507f77e97b6b92fd4104
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2024-10-03 11:46:50 +0100

    Update feedback_guidance.py

--- FDS/feedback_guidance.py
+++ FDS/feedback_guidance.py
@@ -7,6 +7,119 @@ exercises = [
                       characters_q1 = 3
                       return characters_q1
                     """
-      }
+      },
+      {
+        "id": "homework1_Q2",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of characters_q2 must be set to the correct answer option which is 1. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 1:
 
+                    def hw1_exercise_2():
+                          characters_q2 = 1
+                        
+                          return characters_q2
+                    """
+      },
+      {
+        "id": "homework1_Q3",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of names_q1 must be set to the correct answer option which is 2. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 2:
+
+                    def hw1_exercise_3():
+                          names_q1 = 2
+                        
+                          return names_q1
+                    """
+      },
+      {
+        "id": "homework1_Q4",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of names_q2 must be set to the correct answer option which is 4. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 4:
+
+                    def hw1_exercise_4():
+                          names_q2 = 4
+                        
+                          return names_q2
+                    """
+      },
+      {
+        "id": "homework1_Q5",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of names_q3 must be set to the correct answer option which is 2. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 2:
+
+                    def hw1_exercise_5():
+                          names_q3 = 2
+                        
+                          return names_q3
+                    """
+      },
+      {
+        "id": "homework1_Q6",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of jobs_q1 must be set to the correct answer option which is 1. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 1:
+
+                    def hw1_exercise_6():
+                          jobs_q1 = 1
+                        
+                          return jobs_q1
+                    """
+      },
+      {
+        "id": "homework1_Q7",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of jobs_q2 must be set to the correct answer option which is 2. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 2:
+
+                    def hw1_exercise_7():
+                          jobs_q2 = 2
+                        
+                          return jobs_q2
+                    """
+      },
+      {
+        "id": "homework1_Q7b",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of jobs_q2 must be set to the correct answer option which is 2. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 2:
+
+                    def hw1_exercise_8():
+                          jobs_q2 = 2
+                        
+                          return jobs_q2
+                    """
+      },
+      {
+        "id": "homework1_Q8",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of biggest_change must be set to the correct answer option which is 57. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 57:
+
+                    def hw1_exercise_8():
+                          biggest_change = 57
+                        
+                          return biggest_change
+                    """
+      },
+      {
+        "id": "homework1_Q9",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of smallest_change_major must be set to the correct answer option which is 1. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 1:
+
+                    def hw1_exercise_9():
+                          smallest_change_major = 1
+                        
+                          return smallest_change_major
+                    """
+      },
+      {
+        "id": "homework1_Q10",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of smallest_change_major must be set to the correct answer option which is 1. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 1:
+
+                    def hw1_exercise_10():
+                          gws_relative_change = (abs(28-17) / 17) * 100
+                          linguistics_relative_change = (abs(67-49) / 49) * 100
+                          rhetoric_relative_change = (abs(56-113) / 113) * 100
+                        
+                          return gws_relative_change, linguistics_relative_change, rhetoric_relative_change
+                    """
+      },
+      {
+        "id": "homework1_Q11",
+        "guidance": """Their work should be exactly the same as this solution. The most important thing is the value of biggest_rel_change_major must be set to the correct answer option which is 1. It’s important to note that the student will not have access to the example function, so no reference should be made to the solution below i.e the correct value being 1:
+
+                    def hw1_exercise_11():
+                          # Assign biggest_rel_change_major to the number corresponding to the major with the biggest relative change.
+                          biggest_rel_change_major = 1
+                        
+                          return biggest_rel_change_major
+                        
+                    """
+      }
     ]
